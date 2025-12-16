27df2b8e888b44ba3b0b539fe09978d1f456119e
angelop47 <pescettoangelo@gmail.com>
2025-12-16 11:31:31 -0300

    feat: add userRoutes to app for user management

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/auth.routes';
+import userRoutes from './routes/users.routes';
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
 
 export default app;
