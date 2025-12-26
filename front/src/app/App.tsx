2894a1169ba308caa4648d9bcf653b54681b7e45
angelop47 <pescettoangelo@gmail.com>
2025-12-26 11:40:36 -0300

    feat(web): restructure App component to include authentication routes and context

--- front/src/app/App.tsx
+++ front/src/app/App.tsx
@@ -1,12 +1,26 @@
-function App() {
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from '../context/AuthContext';
+import Login from '../pages/auth/Login';
+import Dashboard from '../pages/dashboard/Dashboard';
+import ProtectedRoute from '../routes/ProtectedRoute';
 
+function App() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
-  )
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+
+          {/* Rutas Protegidas */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
