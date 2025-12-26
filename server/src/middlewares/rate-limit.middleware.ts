83580059db49cc03c61c939ee71178b1e08e4ba5
angelop47 <pescettoangelo@gmail.com>
2025-12-26 11:02:51 -0300

    feat(server): update rate limit middleware error message format.

--- server/src/middlewares/rate-limit.middleware.ts
+++ server/src/middlewares/rate-limit.middleware.ts
@@ -7,7 +7,9 @@ import rateLimit from 'express-rate-limit';
 export const authLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutos
     max: 5, // 5 intentos
-    message: 'Too many attempts, please try again later',
+    message: {
+        message: 'Too many attempts, please try again later',
+    },
     standardHeaders: true,
     legacyHeaders: false,
 });
